cad568ff80c4a835165e7e5d511fb15d6d5701df
Diogo Martins de Assis <diogodmda@gmail.com>
2025-01-08 23:02:17 -0300

    refactor: removing ? caracter from loginrequest and registerrequest model

--- eCommerceSolution.UsersService/eCommerce.API/Controllers/AuthController.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Controllers/AuthController.cs
@@ -9,18 +9,16 @@ namespace eCommerce.API.Controllers;
 public class AuthController(IUsersService usersService) : ControllerBase
 {
     [HttpPost("register")] //POST api/auth/register/
-    public async Task<IActionResult> Register(RegisterRequest? registerRequest)
+    public async Task<IActionResult> Register(RegisterRequest registerRequest)
     {
-        if(registerRequest == null) return BadRequest("Invalid registration data");
         var result = await usersService.Register(registerRequest);
         if(result is not { Success: true }) return BadRequest("Invalid registration data");
         return Ok(result);
     }
 
     [HttpPost("login")] //POST api/auth/login/
-    public async Task<IActionResult> Login(LoginRequest? loginRequest)
+    public async Task<IActionResult> Login(LoginRequest loginRequest)
     {
-        if(loginRequest == null) return BadRequest("Invalid login data");
         var result = await usersService.Login(loginRequest);
         if(result is not { Success: true }) return Unauthorized(result);
         return Ok(result);
